f7a77d93dcaf4c744941c4170a60951a8ad206f4
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-03 13:03:58 -0700

    Datos anteriores

--- ListasCirculares/Program.cs
+++ ListasCirculares/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using ListasCirculares;
+
+Lista listas = new Lista();
+
+Console.WriteLine("Mostrando elementos de la lista");
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Tres");
+Console.WriteLine(listas.RecorrerLista());
+Console.WriteLine("---------------------------------------------------------");
+
+Console.WriteLine();
+Console.WriteLine("Buscando Nodo Dos");
+Nodo nodoBusqueda = listas.Buscar("Dos");
+Console.WriteLine(nodoBusqueda.Valor);
+Console.WriteLine("--------------------------------------------------------");
+
+Console.WriteLine("Buscando nodo anterior al Dos");
+Nodo nodoDos = listas.Buscar("Dos");
+Console.WriteLine(nodoDos.Ant.Valor);
+Console.WriteLine("--------------------------------------------------------");
+
+Console.WriteLine("Borrando Dos");
+listas.BorrarNodo("Dos");
+Console.WriteLine(listas.RecorrerLista());
